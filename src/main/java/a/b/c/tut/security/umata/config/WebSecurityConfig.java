de6fe8c5041b047f0b2f46e162cfd672a34607a9
alfu32 <ioan.alferaru@gmail.com>
2020-07-01 11:26:15 +0200

    Kapitel(zwei): thymeleaf, custom login seiten
    https://labs.micromata.de/category/best-practices/tutorial-spring-security/

--- src/main/java/a/b/c/tut/security/umata/config/WebSecurityConfig.java
+++ src/main/java/a/b/c/tut/security/umata/config/WebSecurityConfig.java
@@ -19,14 +19,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/secure/*").authenticated()
-                .anyRequest().permitAll()
-                .and().formLogin().permitAll()
-                .and().logout().permitAll()
-                .and()
-                .csrf().disable();
-        // super.configure(http);
+        http.authorizeRequests().antMatchers("/noSecurity").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
     }
 }
--- src/main/java/a/b/c/tut/security/umata/web/DefaultController.java
+++ src/main/java/a/b/c/tut/security/umata/web/DefaultController.java
@@ -1,18 +1,25 @@
 package a.b.c.tut.security.umata.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DefaultController {
 
-    @RequestMapping("/secure/index")
+    @RequestMapping("/")
     public String index() {
-        return "You can only see this if you're logged in !";
+        return "index";
     }
 
-    @RequestMapping("/pub/index")
+    @RequestMapping("/noSecurity")
     public String noSecurity(){
-        return "Everybody can see this !";
+        return "noSecurity";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "login";
     }
 }
