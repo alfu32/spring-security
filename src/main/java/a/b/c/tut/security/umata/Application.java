625454defdde0aa166c1a9ed2b9946ecfdb683f1
alfu32 <ioan.alferaru@gmail.com>
2020-07-02 14:26:13 +0200

    Kapitel(fünf): h2 console settings anpassen
    https://labs.micromata.de/best-practices/tutorial-spring-security-https-mit-spring-boot/

--- src/main/java/a/b/c/tut/security/umata/Application.java
+++ src/main/java/a/b/c/tut/security/umata/Application.java
@@ -1,15 +1,57 @@
 package a.b.c.tut.security.umata;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryCustomizer;
+import org.springframework.boot.autoconfigure.web.servlet.TomcatServletWebServerFactoryCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.AbstractServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({ "a.b.c.tut.security.umata" })
 public class Application {
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Value("${server.http.port}")
+	private int serverHttpPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServletWebServerFactory createAdditionalTomcatConnector(){
+		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection securityCollection = new SecurityCollection();
+				securityCollection.addPattern("/*");
+				securityConstraint.addCollection(securityCollection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createHttpConnector());
+		return tomcatServletWebServerFactory;
+	}
+
+	private Connector createHttpConnector() {
+		Connector httpConnector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		httpConnector.setScheme("http");
+		httpConnector.setPort(8080);
+		httpConnector.setSecure(false);
+		httpConnector.setRedirectPort(8443);
+		return httpConnector;
 	}
 
 }
